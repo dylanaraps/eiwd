d0e76fda033a16eb331ff2edb0418111d5b4c58c
Denis Kenzior <denkenz@gmail.com>
2014-07-15 18:40:40 -0500

    unit: Fix style issue

--- unit/test-ie.c
+++ unit/test-ie.c
@@ -54,10 +54,9 @@ static void ie_test(const void *data)
 	ie_tlv_iter_init(&iter, test->buf, test->len);
 
 	while (ie_tlv_iter_next(&iter)) {
-
 		ie = (struct ie *)&test->buf[pos];
 		printf("IE %d [%d/%d/%s]\n", count, ie->type, ie->len,
-			l_util_hexstring(&test->buf[pos+2], ie->len));
+			l_util_hexstring(&test->buf[pos + 2], ie->len));
 
 		assert(iter.tag == test->buf[pos++]);
 		assert(iter.len == test->buf[pos++]);
