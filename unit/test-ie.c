c71b70863c8f686dcb9acc8e745643105d53f9c6
Denis Kenzior <denkenz@gmail.com>
2015-01-15 21:49:16 -0600

    unit: Add test case #3 for RSNE parser

--- unit/test-ie.c
+++ unit/test-ie.c
@@ -304,6 +304,20 @@ static const struct ie_rsne_info_test ie_rsne_info_test_2 = {
 	.akm_suites = IE_RSN_AKM_SUITE_8021X,
 };
 
+/* 802.11, Section 8.4.2.27.1; second example */
+static const unsigned char rsne_data_3[] = {
+	0x30, 0x14, 0x01, 0x00, 0x00, 0x0f, 0xac, 0x04, 0x01, 0x00, 0x00, 0x0f,
+	0xac, 0x04, 0x01, 0x00, 0x00, 0x0f, 0xac, 0x01, 0x01, 0x00,
+};
+
+static const struct ie_rsne_info_test ie_rsne_info_test_3 = {
+	.data = rsne_data_3,
+	.data_len = sizeof(rsne_data_3),
+	.group_cipher = IE_RSN_CIPHER_SUITE_CCMP,
+	.pairwise_ciphers = IE_RSN_CIPHER_SUITE_CCMP,
+	.akm_suites = IE_RSN_AKM_SUITE_8021X,
+};
+
 static void ie_test_rsne_info(const void *data)
 {
 	const struct ie_rsne_info_test *test = data;
@@ -333,6 +347,8 @@ int main(int argc, char *argv[])
 				ie_test_rsne_info, &ie_rsne_info_test_1);
 	l_test_add("/ie/RSN Info Parser/Test Case 2",
 				ie_test_rsne_info, &ie_rsne_info_test_2);
+	l_test_add("/ie/RSN Info Parser/Test Case 3",
+				ie_test_rsne_info, &ie_rsne_info_test_3);
 
 	return l_test_run();
 }
