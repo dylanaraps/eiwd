b2bcf97373dfe514d0e37ba36ec7c99fbd17d17f
Denis Kenzior <denkenz@gmail.com>
2016-06-06 18:25:12 -0500

    scan: Remove unused scan_bss_address_to_string

--- src/scan.h
+++ src/scan.h
@@ -79,7 +79,6 @@ void scan_sched_start(struct l_genl_family *nl80211, uint32_t ifindex,
 enum security scan_get_security(enum ie_bss_capability bss_cap,
 						const struct ie_rsn_info *info);
 void scan_bss_free(struct scan_bss *bss);
-const char *scan_bss_address_to_string(const struct scan_bss *bss);
 int scan_bss_rank_compare(const void *a, const void *b, void *user);
 
 void bss_get_supported_ciphers(struct scan_bss *bss,
