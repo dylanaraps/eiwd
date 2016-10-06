91059e68cd03c7788ebde7a8569c3faaaadc9c75
Tim Kourt <tim.a.kourt@linux.intel.com>
2016-10-06 11:06:14 -0700

    auto-t: Add functions to hostapd_cli

--- autotests/util/hostapd.py
+++ autotests/util/hostapd.py
@@ -6,3 +6,11 @@ class HostapdCLI:
     @staticmethod
     def wps_push_button():
         os.system('hostapd_cli wps_pbc')
+
+    @staticmethod
+    def deauthenticate(client_address):
+        os.system('hostapd_cli deauthenticate ' + client_address)
+
+    @staticmethod
+    def kill_all():
+        os.system('killall hostapd')
