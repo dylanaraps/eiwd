14dcda4d595708f44130aa00aac3cfabda1243d3
James Prestwood <james.prestwood@linux.intel.com>
2017-08-21 14:09:07 -0700

    autotest: EAP-AKA autotest
    Implemented milenage algorithm in hlrauc.py. Unlike EAP-SIM, the
    authentication center must compute several values to give back
    to the server (hostapd). This was already done by IWD as the peer
    in EAP-AKA, but was also needed on the server side (HLR AuC).

--- autotests/util/hlrauc.py
+++ autotests/util/hlrauc.py
@@ -54,6 +54,116 @@ class AuthCenter:
             response += (' ' + data)*int(num_chals)
 
             return response
+        elif data[:12] == "AKA-REQ-AUTH":
+            # AKA requests must compute the milenage parameters for the IMSI
+            imsi = data.split(' ')[1]
+            data = self._database[imsi]
+
+            k, opc, amf, sqn = data.split(':')
+
+            rand = self._bytetostring(os.urandom(16))
+
+            response = "AKA-RESP-AUTH %s " % imsi
+
+            return response + self._get_milenage(opc, k, rand, sqn, amf)
+
+    def _bytetostring(self, b):
+        return ''.join(format(x, '02x') for x in b)
+
+    def _xor(self, a, b):
+        ret = bytearray(16)
+        for i in range(len(a)):
+            ret[i] = a[i] ^ b[i]
+        return ret
+
+    def _get_milenage(self, opc, k, rand, sqn, amf):
+        '''
+            Computes milenage values from OPc, K, RAND, SQN and AMF
+            Returns a concatenated list (RAND + AUTN + IK + CK + RES) that
+            will be sent back as the response to the client (hostapd). This
+            is a python re-write of the function eap_aka_get_milenage() from
+            src/simutil.c
+        '''
+        opc = bytearray.fromhex(opc)
+        k = bytearray.fromhex(k)
+        # rand gets returned, so it should be left as a hex string
+        _rand = bytearray.fromhex(rand)
+        sqn = bytearray.fromhex(sqn)
+        amf = bytearray.fromhex(amf)
+
+        aes1 = AES.new(bytes(k), AES.MODE_ECB)
+        tmp1 = self._xor(_rand, opc)
+        tmp1 = aes1.encrypt(bytes(tmp1))
+        tmp1 = bytearray(tmp1)
+
+        tmp2 = bytearray()
+        tmp2[0:6] = sqn
+        tmp2[6:2] = amf
+        tmp2[9:6] = sqn
+        tmp2[15:2] = amf
+
+        tmp3 = bytearray(16)
+        for i in range(len(tmp1)):
+            tmp3[(i + 8) % 16] = tmp2[i] ^ opc[i]
+
+        tmp3 = self._xor(tmp3, tmp1)
+
+        aes2 = AES.new(bytes(k), AES.MODE_ECB)
+        tmp1 = aes2.encrypt(bytes(tmp3))
+        tmp1 = bytearray(tmp1)
+
+        tmp1 = self._xor(tmp1, opc)
+        maca = self._bytetostring(tmp1)
+
+        tmp1 = self._xor(_rand, opc)
+        aes3 = AES.new(bytes(k), AES.MODE_ECB)
+        tmp2 = aes3.encrypt(bytes(tmp1))
+        tmp2 = bytearray(tmp2)
+
+        tmp1 = self._xor(tmp2, opc)
+        tmp1[15] ^= 1
+
+        aes4 = AES.new(bytes(k), AES.MODE_ECB)
+        tmp3 = aes4.encrypt(bytes(tmp1))
+        tmp3 = bytearray(tmp3)
+
+        tmp3 = self._xor(tmp3, opc)
+
+        res = self._bytetostring(tmp3[8:16])
+        ak = self._bytetostring(tmp3[0:6])
+
+        for i in range(len(tmp1)):
+            tmp1[(i + 12) % 16] = tmp2[i] ^ opc[i]
+
+        tmp1[15] ^= 1 << 1
+        aes5 = AES.new(bytes(k), AES.MODE_ECB)
+        tmp1 = aes5.encrypt(bytes(tmp1))
+        tmp1 = bytearray(tmp1)
+
+        tmp1 = self._xor(tmp1, opc)
+        ck = self._bytetostring(tmp1)
+
+        for i in range(len(tmp1)):
+            tmp1[(i + 8) % 16] = tmp2[i] ^ opc[i]
+
+        tmp1[15] ^= 1 << 2
+        aes6 = AES.new(bytes(k), AES.MODE_ECB)
+        tmp1 = aes6.encrypt(bytes(tmp1))
+        tmp1 = bytearray(tmp1)
+        tmp1 = self._xor(tmp1, opc)
+        ik = self._bytetostring(tmp1)
+
+        tmp1 = bytearray.fromhex(ak)
+        autn = bytearray(6)
+        for i in range(0, 6):
+            autn[i] = sqn[i] ^ tmp1[i]
+
+        autn[6:2] = amf
+        autn[8:8] = bytearray.fromhex(maca)[0:8]
+
+        autn = self._bytetostring(autn)
+
+        return rand + ' ' + autn + ' ' + ik + ' ' + ck + ' ' + res
 
     def stop(self):
         '''
