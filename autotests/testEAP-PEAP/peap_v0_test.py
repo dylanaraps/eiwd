2dd84f011480c1e9471da055edd7e337e4b0094c
Tim Kourt <tim.a.kourt@linux.intel.com>
2018-02-23 16:04:11 -0800

    auto-t: add tests for Protected EAP - type 25

---
+++ autotests/testEAP-PEAP/peap_v0_test.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+
+import unittest
+import sys
+import time
+
+sys.path.append('../util')
+import iwd
+from iwd import IWD
+from iwd import NetworkType
+
+class Test(unittest.TestCase):
+
+    def test_connection_success(self):
+        ssid_to_connect = 'ssidEAP-PEAPv0'
+
+        wd = IWD(True)
+        wd.wait(1)
+
+        devices = wd.list_devices();
+
+        self.assertIsNotNone(devices)
+        device = devices[0]
+
+        condition = 'not obj.scanning'
+        wd.wait_for_object_condition(device, condition)
+
+        device.scan()
+
+        condition = 'not obj.scanning'
+        wd.wait_for_object_condition(device, condition)
+
+        ordered_networks = device.get_ordered_networks()
+        ordered_network = None
+
+        for o_n in ordered_networks:
+            if o_n.name == ssid_to_connect:
+                ordered_network = o_n
+                break
+
+        self.assertEqual(ordered_network.name, ssid_to_connect)
+
+        self.assertEqual(ordered_network.type, NetworkType.eap)
+
+        condition = 'not obj.connected'
+        wd.wait_for_object_condition(ordered_network.network_object, condition)
+
+        ordered_network.network_object.connect()
+
+        condition = 'obj.connected'
+        wd.wait_for_object_condition(ordered_network.network_object, condition)
+
+        device.disconnect()
+
+        condition = 'not obj.connected'
+        wd.wait_for_object_condition(ordered_network.network_object, condition)
+
+        del wd
+
+    @classmethod
+    def setUpClass(cls):
+        IWD.copy_to_storage('ssidEAP-PEAPv0.8021x')
+
+    @classmethod
+    def tearDownClass(cls):
+        IWD.clear_storage()
+
+if __name__ == '__main__':
+    unittest.main(exit=True)
