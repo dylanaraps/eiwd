25a9d2a71fb741cfb545fe1f49818e4f34988de7
Andrew Zaborowski <andrew.zaborowski@intel.com>
2018-04-26 11:29:27 +0200

    autotests: Test encrypted private keys with EAP-TLS
    Make 3 connections in test EAP-TLS, one with an unencrypted private key,
    one with the private key passphrase provided in the provisioning file
    and one with the passphrase provided through the agent.  Also improve
    the scanning logic at the beginning.

--- autotests/testEAP-TLS/connection_test.py
+++ autotests/testEAP-TLS/connection_test.py
@@ -6,13 +6,25 @@ import sys
 sys.path.append('../util')
 import iwd
 from iwd import IWD
+from iwd import PSKAgent
 from iwd import NetworkType
 import testutil
+import hostapd
 
 class Test(unittest.TestCase):
 
-    def test_connection_success(self):
+    def do_test_connection_success(self, ssid, passphrase=None):
         wd = IWD()
+
+        if passphrase:
+            psk_agent = PSKAgent(passphrase)
+            wd.register_psk_agent(psk_agent)
+
+        hostapd_ifname = None
+        for ifname in hostapd.hostapd_map:
+            if ssid + '.conf' in hostapd.hostapd_map[ifname].config:
+                hostapd_ifname = ifname
+                break
 
         devices = wd.list_devices();
         self.assertIsNotNone(devices)
@@ -21,15 +33,16 @@ class Test(unittest.TestCase):
         condition = 'not obj.scanning'
         wd.wait_for_object_condition(device, condition)
 
-        device.scan()
-
-        condition = 'not obj.scanning'
-        wd.wait_for_object_condition(device, condition)
+        if not device.get_ordered_networks():
+            device.scan()
+            condition = 'obj.scanning'
+            wd.wait_for_object_condition(device, condition)
+            condition = 'not obj.scanning'
+            wd.wait_for_object_condition(device, condition)
 
         ordered_networks = device.get_ordered_networks()
-        ordered_network = ordered_networks[0]
+        ordered_network = [n for n in ordered_networks if n.name == ssid][0]
 
-        self.assertEqual(ordered_network.name, "ssidEAP-TLS")
         self.assertEqual(ordered_network.type, NetworkType.eap)
 
         condition = 'not obj.connected'
@@ -41,16 +54,30 @@ class Test(unittest.TestCase):
         wd.wait_for_object_condition(ordered_network.network_object, condition)
 
         testutil.test_iface_operstate()
-        testutil.test_ifaces_connected()
+        testutil.test_ifaces_connected(hostapd_ifname, 'wln3')
 
         device.disconnect()
 
         condition = 'not obj.connected'
         wd.wait_for_object_condition(ordered_network.network_object, condition)
 
+        if passphrase:
+            wd.unregister_psk_agent(psk_agent)
+
+    def test_eap_tls(self):
+        self.do_test_connection_success('ssidEAP-TLS')
+
+    def test_eap_tls2(self):
+        self.do_test_connection_success('ssidEAP-TLS2')
+
+    def test_eap_tls3(self):
+        self.do_test_connection_success('ssidEAP-TLS3', 'abc')
+
     @classmethod
     def setUpClass(cls):
         IWD.copy_to_storage('ssidEAP-TLS.8021x')
+        IWD.copy_to_storage('ssidEAP-TLS2.8021x')
+        IWD.copy_to_storage('ssidEAP-TLS3.8021x')
 
     @classmethod
     def tearDownClass(cls):
