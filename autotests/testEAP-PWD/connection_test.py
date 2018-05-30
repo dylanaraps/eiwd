ca0f9933580672cd8f1ecf6c0440c4ce3fb41bfd
Tim Kourt <tim.a.kourt@linux.intel.com>
2018-05-30 16:39:06 -0700

    auto-t: change EAP-PWD to trigger eapol reauth

--- autotests/testEAP-PWD/connection_test.py
+++ autotests/testEAP-PWD/connection_test.py
@@ -9,9 +9,23 @@ from iwd import IWD
 from iwd import PSKAgent
 from iwd import NetworkType
 
+from hostapd import HostapdCLI
+from hostapd import hostapd_map
+
 class Test(unittest.TestCase):
 
     def validate_connection(self, wd):
+
+        hostapd = None
+
+        for hostapd_if in list(hostapd_map.values()):
+            hpd = HostapdCLI(hostapd_if)
+            if hpd.get_config_value('ssid') == 'ssidEAP-PWD':
+                hostapd = hpd
+                break
+
+        self.assertIsNotNone(hostapd)
+
         psk_agent = PSKAgent('eap-pwd-identity', ('eap-pwd-identity',
                                                                   'secret123'))
         wd.register_psk_agent(psk_agent)
@@ -43,6 +57,13 @@ class Test(unittest.TestCase):
         wd.wait_for_object_condition(ordered_network.network_object, condition)
 
         ordered_network.network_object.connect()
+
+        condition = 'obj.connected'
+        wd.wait_for_object_condition(ordered_network.network_object, condition)
+
+        hostapd.eapol_reauth(device.address)
+
+        wd.wait(10)
 
         condition = 'obj.connected'
         wd.wait_for_object_condition(ordered_network.network_object, condition)
