91456b8d98d706118f3fd32fe26fe32ae3b80ff9
Andrew Zaborowski <andrew.zaborowski@intel.com>
2018-06-13 18:58:32 +0200

    autotests: Test 4 ways MsCHAPv2 can be configured

--- autotests/testEAP-MSCHAPV2/connection_test.py
+++ autotests/testEAP-MSCHAPV2/connection_test.py
@@ -10,26 +10,37 @@ from iwd import NetworkType
 
 class Test(unittest.TestCase):
 
-    def test_connection_success(self):
+    def run_connection_test(self, ssid, *secrets):
         wd = IWD()
+
+        psk_agent = iwd.PSKAgent(*secrets)
+        wd.register_psk_agent(psk_agent)
 
         devices = wd.list_devices();
         self.assertIsNotNone(devices)
         device = devices[0]
 
+        try:
+            device.disconnect()
+        except:
+            pass
+
         condition = 'not obj.scanning'
         wd.wait_for_object_condition(device, condition)
 
-        device.scan()
+        if not device.get_ordered_networks():
+            device.scan()
+            condition = 'obj.scanning'
+            wd.wait_for_object_condition(device, condition)
+            condition = 'not obj.scanning'
+            wd.wait_for_object_condition(device, condition)
 
-        condition = 'not obj.scanning'
-        wd.wait_for_object_condition(device, condition)
-
-        ordered_networks = device.get_ordered_networks()
-        ordered_network = ordered_networks[0]
-
-        self.assertEqual(ordered_network.name, "ssidEAP-MSCHAPV2")
-        self.assertEqual(ordered_network.type, NetworkType.eap)
+        network = None
+        for ordered_network in device.get_ordered_networks():
+            if ordered_network.name == ssid:
+                network = ordered_network
+                break
+        self.assertEqual(network.type, NetworkType.eap)
 
         condition = 'not obj.connected'
         wd.wait_for_object_condition(ordered_network.network_object, condition)
@@ -39,14 +50,31 @@ class Test(unittest.TestCase):
         condition = 'obj.connected'
         wd.wait_for_object_condition(ordered_network.network_object, condition)
 
+        wd.unregister_psk_agent(psk_agent)
+
         device.disconnect()
 
         condition = 'not obj.connected'
         wd.wait_for_object_condition(ordered_network.network_object, condition)
 
+    def test_agent_none(self):
+        self.run_connection_test('ssidEAP-MSCHAPV2-1')
+
+    def test_agent_none_hash(self):
+        self.run_connection_test('ssidEAP-MSCHAPV2-2')
+
+    def test_agent_passwd(self):
+        self.run_connection_test('ssidEAP-MSCHAPV2-3', [], ('domain\\User', 'Password'))
+
+    def test_agent_username_and_passwd(self):
+        self.run_connection_test('ssidEAP-MSCHAPV2-4', [], ('domain\\User', 'Password'))
+
     @classmethod
     def setUpClass(cls):
-        IWD.copy_to_storage('ssidEAP-MSCHAPV2.8021x')
+        IWD.copy_to_storage('ssidEAP-MSCHAPV2-1.8021x')
+        IWD.copy_to_storage('ssidEAP-MSCHAPV2-2.8021x')
+        IWD.copy_to_storage('ssidEAP-MSCHAPV2-3.8021x')
+        IWD.copy_to_storage('ssidEAP-MSCHAPV2-4.8021x')
 
     @classmethod
     def tearDownClass(cls):
