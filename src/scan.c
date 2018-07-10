f7a30106bf7228687805b7a99feb9d3ab1a2a2ef
Tim Kourt <tim.a.kourt@linux.intel.com>
2018-07-10 15:46:56 -0700

    scan: initial support for the direct probe request
    This enables the discovery of the hidden networks

--- src/scan.c
+++ src/scan.c
@@ -292,7 +292,14 @@ static struct l_genl_msg *scan_build_cmd(struct scan_context *sc, bool passive,
 
 	if (!passive) {
 		l_genl_msg_enter_nested(msg, NL80211_ATTR_SCAN_SSIDS);
-		l_genl_msg_append_attr(msg, NL80211_ATTR_SSID, 0, NULL);
+
+		if (params->ssid)
+			l_genl_msg_append_attr(msg, NL80211_ATTR_SSID,
+						strlen(params->ssid),
+						params->ssid);
+		else
+			l_genl_msg_append_attr(msg, NL80211_ATTR_SSID, 0, NULL);
+
 		l_genl_msg_leave_nested(msg);
 	}
 
