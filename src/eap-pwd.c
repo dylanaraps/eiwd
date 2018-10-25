a2efe2d2dec7a45cb8c82798fc5993506ffbacb7
Denis Kenzior <denkenz@gmail.com>
2018-10-25 14:52:10 -0500

    eap-pwd: Warn if deprecated setting is used

--- src/eap-pwd.c
+++ src/eap-pwd.c
@@ -728,8 +728,11 @@ static int eap_pwd_check_settings(struct l_settings *settings,
 						"%sPWD-Password", prefix);
 		password = l_settings_get_string(settings, "Security",
 							password_key_old);
-		if (password)
+		if (password) {
+			l_warn("Setting '%s' is deprecated, use '%s' instead",
+					password_key_old, password_key);
 			return 0;
+		}
 
 		secret = l_queue_find(secrets, eap_secret_info_match,
 								password_key);
