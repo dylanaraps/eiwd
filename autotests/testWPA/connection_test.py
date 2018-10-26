ec4cb6beeb14af56c323f1bc0bc191af902a324b
James Prestwood <james.prestwood@linux.intel.com>
2018-10-26 09:45:01 -0700

    auto-t: test connectivity in testWPA
    testWPA was not verifying connectivity between the two interfaces. Funny
    enough, doing this resulted in the same problems that adhoc had where
    we were setting the connection as complete before the gtk/igtk were set.
    This is fixed now so we can now use testutil in this test.

--- autotests/testWPA/connection_test.py
+++ autotests/testWPA/connection_test.py
@@ -8,6 +8,7 @@ import iwd
 from iwd import IWD
 from iwd import PSKAgent
 from iwd import NetworkType
+import testutil
 
 class Test(unittest.TestCase):
 
@@ -41,6 +42,9 @@ class Test(unittest.TestCase):
 
         condition = 'obj.connected'
         wd.wait_for_object_condition(ordered_network.network_object, condition)
+
+        testutil.test_iface_operstate()
+        testutil.test_ifaces_connected()
 
         device.disconnect()
 
