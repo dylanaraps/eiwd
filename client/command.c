5afe4576a1d9ef1aa9f902211d6a8571a71abb87
Tim Kourt <tim.a.kourt@linux.intel.com>
2018-11-09 13:43:14 -0800

    client: Enable 'help', 'version' cmds in non-interactive mode

--- client/command.c
+++ client/command.c
@@ -509,8 +509,14 @@ void command_process_prompt(char **argv, int argc)
 		return;
 
 	if (!interactive_mode) {
+		if (command_match_misc_commands(argv, argc)) {
+			exit_status = EXIT_SUCCESS;
+			goto quit;
+		}
+
 		display_error("Invalid command\n");
 		exit_status = EXIT_FAILURE;
+quit:
 		l_main_quit();
 		return;
 	}
