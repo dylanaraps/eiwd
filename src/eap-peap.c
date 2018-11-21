299af7fc397d4a216bb17c5e9c131c15a37b9027
Andrew Zaborowski <andrew.zaborowski@intel.com>
2018-11-21 16:14:03 +0100

    eap-tls, ttls, peap: Update for private key API changes

--- src/eap-peap.c
+++ src/eap-peap.c
@@ -953,12 +953,10 @@ static int eap_peap_check_settings(struct l_settings *settings,
 	}
 
 	if (path) {
+		struct l_key *priv_key;
 		bool encrypted;
-		uint8_t *priv_key;
-		size_t size;
 
-		priv_key = l_pem_load_private_key(path, passphrase,
-							&encrypted, &size);
+		priv_key = l_pem_load_private_key(path, passphrase, &encrypted);
 		if (!priv_key) {
 			if (!encrypted) {
 				l_error("Error loading client private key %s",
@@ -982,8 +980,7 @@ static int eap_peap_check_settings(struct l_settings *settings,
 					passphrase_entry, NULL, path,
 					EAP_CACHE_TEMPORARY);
 		} else {
-			memset(priv_key, 0, size);
-			l_free(priv_key);
+			l_key_free(priv_key);
 
 			if (passphrase && !encrypted) {
 				l_error("%s present but client private "
