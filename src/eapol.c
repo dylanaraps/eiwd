869cac4bec9de45b6c346ffca16b7b032a21da81
James Prestwood <james.prestwood@linux.intel.com>
2019-05-03 11:59:52 -0700

    eapol: remove eapol_set_started, no longer needed

--- src/eapol.c
+++ src/eapol.c
@@ -2306,11 +2306,6 @@ void __eapol_set_rekey_offload_func(eapol_rekey_offload_func_t func)
 	rekey_offload = func;
 }
 
-void eapol_set_started(struct eapol_sm *sm)
-{
-	sm->started = true;
-}
-
 void eapol_register(struct eapol_sm *sm)
 {
 	l_queue_push_head(state_machines, sm);
