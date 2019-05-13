d6eade2252d306270be08604d060f7c97199d898
James Prestwood <james.prestwood@linux.intel.com>
2019-05-13 14:29:26 -0700

    auto-t: add test for CCMP with no support
    Using the new cipher_disable hwsim option we can disable CCMP support
    but require it on the AP. This should result in a Not Supported error.

---
+++ autotests/testWPA2-no-CCMP/connection_test.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+import unittest
+import sys
+
+sys.path.append('../util')
+import iwd
+from iwd import IWD
+from iwd import PSKAgent
+from iwd import NetworkType
+import testutil
+
+class Test(unittest.TestCase):
+
+    def test_connection_success(self):
+        wd = IWD()
+
+        psk_agent = PSKAgent("secret123")
+        wd.register_psk_agent(psk_agent)
+
+        devices = wd.list_devices(1)
+        device = devices[0]
+
+        condition = 'not obj.scanning'
+        wd.wait_for_object_condition(device, condition)
+
+        device.scan()
+
+        condition = 'not obj.scanning'
+        wd.wait_for_object_condition(device, condition)
+
+        ordered_network = device.get_ordered_network('ssidCCMP')
+
+        self.assertEqual(ordered_network.type, NetworkType.psk)
+
+        condition = 'not obj.connected'
+        wd.wait_for_object_condition(ordered_network.network_object, condition)
+
+        with self.assertRaises(iwd.NotSupportedEx):
+            ordered_network.network_object.connect()
+
+        wd.unregister_psk_agent(psk_agent)
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        IWD.clear_storage()
+
+if __name__ == '__main__':
+    unittest.main(exit=True)
