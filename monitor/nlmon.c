dylanaraps/eiwd

commit 3ff9f59ba1a7b6edc6fe2c1168dca0aac90c072a

monitor: Print MAC_ADDRS array

monitor/nlmon.c
@@ -3860,7 +3860,8 @@ static const struct attr_entry attr_table[] = {
 	{ NL80211_ATTR_ACL_POLICY,
 			"ACL Policy", ATTR_U32 },
 	{ NL80211_ATTR_MAC_ADDRS,
-			"MAC Addresses" },
+			"MAC Addresses", ATTR_ARRAY,
+					{ .array_type = ATTR_ADDRESS } },
 	{ NL80211_ATTR_MAC_ACL_MAX,
 			"MAC ACL Max" },
 	{ NL80211_ATTR_RADAR_EVENT,
@@ -3977,6 +3978,8 @@ static void print_value(int indent, const char *label, enum attr_type type,
 	case ATTR_S64:
 	case ATTR_STRING:
 	case ATTR_ADDRESS:
+		print_address(indent, label, buf);
+		break;
 	case ATTR_BINARY:
 	case ATTR_NESTED:
 	case ATTR_ARRAY:

commit cd67e66f8e73f9f1c6c1d73f01981e47a9e150d3

monitor: Fix ARRAY output
Commit 3ff9f59ba1a7 ("monitor: Print MAC_ADDRS array") introduced a
small bug where some arguments were erroneously printed as an address.

monitor/nlmon.c
@@ -3969,6 +3969,9 @@ static void print_value(int indent, const char *label, enum attr_type type,
 		if (len != 4)
 			printf("malformed packet\n");
 		break;
+	case ATTR_ADDRESS:
+		print_address(indent, label, buf);
+		break;
 	case ATTR_UNSPEC:
 	case ATTR_FLAG:
 	case ATTR_U8:
@@ -3977,9 +3980,6 @@ static void print_value(int indent, const char *label, enum attr_type type,
 	case ATTR_S32:
 	case ATTR_S64:
 	case ATTR_STRING:
-	case ATTR_ADDRESS:
-		print_address(indent, label, buf);
-		break;
 	case ATTR_BINARY:
 	case ATTR_NESTED:
 	case ATTR_ARRAY: