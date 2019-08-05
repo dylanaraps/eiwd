5af5498aa9fb38976128ea2e8cfdd61327b8fa17
James Prestwood <james.prestwood@linux.intel.com>
2019-08-05 11:17:46 -0700

    auto-t: add shell test
    This is merely an empty test that can act as a sandbox for the new
    --shell command. It was not named with 'test' so that autotesting
    will skip it.
    
    This test is not very useful for virtual hardware testing
    (mac80211_hwsim), but very useful for USB/PCI passthrough. When
    setup correctly, you can now pass through a single device and test
    against real networks with a minimal kernel.

---
+++ autotests/shell/shell_test.py
@@ -0,0 +1,20 @@
+import unittest
+import sys
+
+sys.path.append('../util')
+
+class Test(unittest.TestCase):
+
+    def test_connection_success(self):
+        pass
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+if __name__ == '__main__':
+    unittest.main(exit=True)
