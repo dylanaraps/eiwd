c1a9f71b39646b532d7ff26001d31fbe0de417ad
Marcel Holtmann <marcel@holtmann.org>
2019-09-08 22:10:28 +0200

    build: Add script to start iwd.service via systemd-run

---
+++ tools/run-iwd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+systemd-run \
+	--pty --wait --collect \
+	--unit=iwd.service \
+	--service-type=dbus \
+	--property=BusName=net.connman.iwd \
+	--property=NotifyAccess=main \
+	--property=LimitNPROC=1 \
+	--property=Restart=on-failure \
+	--property=PrivateTmp=true \
+	--property=NoNewPrivileges=true \
+	--property=DevicePolicy=closed \
+	--property=DeviceAllow=/dev/rfkill\ rw \
+	--property=ProtectHome=yes \
+	--property=ProtectSystem=strict \
+	--property=ProtectControlGroups=yes \
+	--property=ProtectKernelModules=yes \
+	--property=StateDirectory=iwd \
+	--property=ConfigurationDirectory=iwd \
+	./src/iwd $*
