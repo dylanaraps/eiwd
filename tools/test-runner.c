c0863e5bc62f645f0bde144e4e906b7277866bf7
Tim Kourt <tim.a.kourt@linux.intel.com>
2019-10-22 16:13:13 -0700

    t-runner: Fix mem leak caused by python test names

--- tools/test-runner.c
+++ tools/test-runner.c
@@ -1089,6 +1089,8 @@ static bool add_path(const char *path, int level, struct l_queue *config_queue)
 		l_queue_insert(config_queue, entry, insert_test_entry, NULL);
 	}
 
+	l_queue_destroy(py_test_queue, l_free);
+
 	closedir(dir);
 	return true;
 }
