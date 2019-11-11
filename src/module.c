95d4eea68f66890458e2ce62f2a53635045f5f89
Andrew Zaborowski <andrew.zaborowski@intel.com>
2019-11-11 13:34:17 +0100

    modules: Unload modules in reverse order from loading
    Make sure a module A that is needed by B is not unloaded before B.

--- src/module.c
+++ src/module.c
@@ -173,7 +173,7 @@ void iwd_modules_exit()
 		return;
 
 	for (i = 0; i < n_modules; i++) {
-		desc = modules_sorted[i];
+		desc = modules_sorted[n_modules - 1 - i];
 		if (!desc->active)
 			continue;
 
