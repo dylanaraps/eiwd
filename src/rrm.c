002fdb5c10c7753c42be973f480759df33153ca7
James Prestwood <prestwoj@gmail.com>
2019-11-21 12:07:00 -0800

    rrm: fix non-ascii character in comment

--- src/rrm.c
+++ src/rrm.c
@@ -274,7 +274,7 @@ static size_t build_report_for_bss(struct rrm_beacon_req_info *beacon,
 	 * 802.11 9.4.2.22.7 Beacon report
 	 *
 	 * "The Parent TSF field contains the lower 4 octets of the measuring
-	 *  STA’s TSF timer value"
+	 *  STA's TSF timer value"
 	 */
 	l_put_le32((uint32_t)(bss->parent_tsf & 0xffffffff), to);
 	to += 4;
