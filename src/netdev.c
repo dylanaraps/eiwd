dylanaraps/eiwd

commit 0238ffb8d9991a680e1aafa45aaa74937baaa218

netdev: Use -EOPNOTSUPP instead of -ENOTSUPP
The kernel uses -EOPNOTSUPP in the case of change_station operation not
being provided.  On most systems -EOPNOTSUPP is defined to be the same
as -ENOTSUPP, but seemingly not all systems.

src/netdev.c
@@ -1082,7 +1082,7 @@ static void netdev_set_station_cb(struct l_genl_msg *msg, void *user_data)
 		return;
 
 	err = l_genl_msg_get_error(msg);
-	if (err == -ENOTSUPP)
+	if (err == -EOPNOTSUPP)
 		goto done;
 
 	if (err < 0) {

commit 3607ee0c7ee78b889f59312262466db5d38b03ef

netdev: Allow both -EOPNOTSUPP and -ENOTSUPP
It seems that the kernel uses -EOPNOTSUPP if the change_station
operation is not implemented by the driver.  However, some drivers do
implement change_station and choose to report -ENOTSUPP instead of
-EOPNOTSUPP.

To add to the confusion, EOPNOTSUPP and -ENOTSUPP are the same on some
systems (e.g. Gentoo).  Be paranoid and allow both errors to be ignored
when sending CMD_SET_STATION.

Fixes: 0238ffb8d999 ("netdev: Use -EOPNOTSUPP instead of -ENOTSUPP")

src/netdev.c
@@ -1082,7 +1082,7 @@ static void netdev_set_station_cb(struct l_genl_msg *msg, void *user_data)
 		return;
 
 	err = l_genl_msg_get_error(msg);
-	if (err == -EOPNOTSUPP)
+	if (err == -EOPNOTSUPP || err == -ENOTSUPP)
 		goto done;
 
 	if (err < 0) {